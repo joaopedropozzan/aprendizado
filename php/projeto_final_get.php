ae4c5967bc7f31b77401118bed5ba045dd786abc
João Pedro Pozzan <joaomagri17@gmail.com>
2025-10-17 11:28:23 -0300

    adicionado o gitignore

--- php/projeto_final_get.php
+++ php/projeto_final_get.php
@@ -1,3 +1,22 @@
 <?php
-    include_once('conexao.php'); // once signfica que ele é executado uma vez somente
+global $conexao;
+include_once('conexao.php'); // once signfica que ele é executado uma vez somente
+    //Vamos montar o select
+    // 1ª situação - SEM RECEBER O ID por GET
+    // 2ª situação - RECEBENDO O ID por GET
+    $stmt = $conexao->prepare("SELECT * FROM projeto"); //Prepara a query
+    $stmt->execute(); // Executa a query
+    $resultado = $stmt->get_result(); // Pega o resultado
+    $tabela = []; // array que enviara ao front
 
+    if($resultado->num_rows > 0){
+        // criar o laço de repetição para ler o resultado
+        while ($linha = $resultado->fetch_assoc()){
+            $tabela[] = $linha;
+        }
+    }else{
+        echo 'não encontrou nenhum registro!';
+    }
+    echo '<pre>';
+    var_dump($tabela);
+    echo '</pre>';
